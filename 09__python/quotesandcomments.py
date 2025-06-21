e0bc0b9277f511085ce151320b950e6dd9a4b6d7
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-06-21 14:21:30 +0500

    comments

---
+++ 09__python/quotesandcomments.py
@@ -0,0 +1,14 @@
+# this is a single line comment
+"""
+this is a multiline
+comment
+"""
+
+# string
+skill = "DevOps"
+# multiline print
+print("""
+this is a print 
+where we are using multiline
+""")
+print(skill)
