3bcbbf15605c1d0ca381f26f01b5b6452919c720
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-06-27 06:33:59 +0000

    updated devcontainer

--- devcontainer-features.sh
+++ devcontainer-features.sh
@@ -4,6 +4,14 @@ devcontainer templates apply \
 	--template-id ghcr.io/devcontainers/templates/python:latest \
 	--template-args '{ "variant": "debian-bookworm" }' \
 	--features '[
+
+"ghcr.io/devcontainers/features/common-utils:2": {
+	"username": "shakir",      
+	"userUid": "1000",
+	"userGid": "1000",
+	"upgradePackages": "true"
+	"installZsh": true
+}
 { "id": "ghcr.io/devcontainers-extra/features/neovim-homebrew:1" },
 	{ "id":  "ghcr.io/va-h/devcontainers-features/uv:1": },
 
