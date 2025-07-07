3ac6b053f3c087049a1e6f2e378e2cfc9eca3246
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-07-07 08:44:20 +0500

    print py

---
+++ 09__python/01basics/print.py
@@ -0,0 +1,8 @@
+# Printing of variables
+name = "DevOps"
+disease = "covid19"
+family = "sars_cov_19"
+skill = "teraform"
+print("I am learning ", name, "right now")
+print("i am a very good at {}".format(skill))
+print(f"The name of the virus is {disease} and it belongs to the family of {family}")
