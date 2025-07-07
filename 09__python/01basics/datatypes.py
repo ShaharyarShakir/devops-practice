3e8a2031c58dce0d4a4ad25f364e543de73b4d18
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-07-07 08:43:07 +0500

    datatypes py

---
+++ 09__python/01basics/datatypes.py
@@ -0,0 +1,10 @@
+# list => collection of datatypes inclosed in square brackets => mutable
+list = [1, 2, "devops", "terraform"]
+
+print(list)
+
+# tuples => collection of datatypes inclosed in curly brackets => mutable
+tuple = (1, 2, "devops", "terraform")
+print(tuple)
+
+# Dictonary => key value pair
