aac551569e0b20a3d14f7ee4a12afa08079f9a6e
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-07-07 08:45:15 +0500

    loops py

---
+++ 09__python/01basics/loops.py
@@ -0,0 +1,17 @@
+# for loop
+
+name = "shakir"
+
+for i in name:
+    print(i)
+
+diseases = ("smallpox", "cowpox", "malaria", "HIV")
+
+for disease in diseases:
+    print("Name of the disease is: ", disease)
+
+x = 0
+while x <= 10:
+    print("The value of x is: ", x)
+    x += 1
+print("End of loop")
