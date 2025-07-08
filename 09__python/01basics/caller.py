fcd76619aa70f256920ac1566bdcdd5a14382ee8
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-07-08 05:07:51 +0500

    caller py

---
+++ 09__python/01basics/caller.py
@@ -0,0 +1,7 @@
+import module
+# from module import vac_feedback # => import a specfice function
+# from module import *  # => import all the functions
+# print(dir(module))
+
+
+module.order_food("Salad", "pizza", "burger", "biryani", "zinger")
