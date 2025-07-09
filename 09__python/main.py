48448433cad1bd0e63168769e197cafc688b51ad
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-07-09 09:28:03 +0500

    deleted

--- 09__python/main.py
+++
@@ -1,8 +0,0 @@
-x = 0
-
-print("Indentation")
-
-if x == 0:
-    print("The value of x is zero")
-else:
-    print("the value of x is not zero")
--- 09__python/variables.py
+++
@@ -1,9 +0,0 @@
-a = "name"
-b = 2
-c = 2.5
-
-u = v = w = 10
-x, y, z = "aplha", "beta", "gamma"
-
-print(f"{a}\n{b}\n{c}")
-print(f"{x}\n{y}\n{z}")
