0c30cdd5e894cd1979fba5266b05238d6262636e
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-08-15 11:31:19 +0500

    added dev variables

---
+++ 15__terraform/practice/yt-exercise/cloud-providers/aws/ec2/variables.tf
@@ -0,0 +1,26 @@
+variable "ec2_instance_type" {
+  description = "Type of EC2 instance to create"
+  type        = string
+  default     = "t2.micro"
+}
+variable "ec2_default_root_storage_size" {
+  description = "Size of the root block device in GB"
+  type        = number
+  default     = 8
+}
+variable "ec2_ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0de716d6197524dd9"
+}
+variable "ec2_storage_type" {
+  description = "Type of storage for the EC2 instance"
+  type        = string
+  default     = "gp3"
+}
+variable "env" {
+  description = "Environment type (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+
+}
